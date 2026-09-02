d7342cd460b18776914abc008cb4708899e31e74
JK <joe.keaveney@googlemail.com>
2026-09-02 12:07:22 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Link from "next/link";
 import AuthNav from "@/components/AuthNav";
+import MessagesNav from "@/components/MessagesNav";
 import ThemeToggle from "@/components/ThemeToggle";
 
 export default function RootLayout({
@@ -15,45 +16,34 @@ export default function RootLayout({
           <div className="wrap head">
             <Link className="brand" href="/">
               <span className="brandRC">RC</span>
-
               <span className="brandWords">
                 ENDURANCE
                 <br />
                 SERIES
               </span>
-
-              <span className="brandFlag">
-                ▰▰▰
-              </span>
+              <span className="brandFlag">▰▰▰</span>
             </Link>
 
-            <nav className="navs">
-              <Link
-                className="nav"
-                href="/"
-              >
-                Home
-              </Link>
+            <div className="navArea">
+              <nav className="navs">
+                <Link className="nav" href="/">
+                  Home
+                </Link>
 
-              <Link
-                className="nav"
-                href="/championship"
-              >
-                Series Details
-              </Link>
+                <Link
+                  className="nav"
+                  href="/championship"
+                >
+                  Series Details
+                </Link>
 
-              <Link
-                className="nav"
-                href="/teams"
-              >
-                Teams
-              </Link>
+                <AuthNav />
+              </nav>
 
-              <AuthNav />
-            </nav>
-
-            <div className="themeToggleArea">
-              <ThemeToggle />
+              <div className="navActions">
+                <MessagesNav />
+                <ThemeToggle />
+              </div>
             </div>
           </div>
         </header>
